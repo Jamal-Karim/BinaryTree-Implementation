05f08b292af7b0ab69d6940ced0c2f811d31c00f
Jamal Karim <1jamalkarim@gmail.com>
2024-05-08 18:45:04 -0700

    Add delete method for binary tree

--- app.py
+++ app.py
@@ -31,6 +31,26 @@ class Tree:
             else:
                 root.left = self.insert(root.left, value)
         return root
+        
+    def delete(self, root, value):
+        if root is None:
+            return None
+        
+        if value < root.data:
+            root.left = self.delete(root.left, value)
+        elif value > root.data:
+            root.right = self.delete(root.right, value)
+        else:
+            if root.left == None:
+                return root.right
+            elif root.right == None:
+                return root.left
+            
+            curr = root.right
+            while curr.left:
+                curr = curr.left
+            root.data = curr.data
+            root.right = self.delete(root.right, root.data)
     
 x = Tree([1, 4, 6, 8, 3, 9, 12])
 
@@ -43,6 +63,3 @@ def pretty_print(node, prefix="", is_left=True):
     print(f"{prefix}{'`-- ' if is_left else '+-- '}{node.data}")
 
     pretty_print(node.left, prefix + ("    " if is_left else "|   "), True)
-
-pretty_print(x.insert(x.root, 7))
-# print(x.insert(7))
